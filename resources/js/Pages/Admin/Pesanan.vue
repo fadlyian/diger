70f264903ad516934a8cdc61be2fcd831dee3d25
khafidhasukma@gmail.com <khafidhasukma@gmail.com>
2023-07-20 06:03:49 +0700

    [add] page pesanan admin

--- resources/js/Pages/Admin/Pesanan.vue
+++ resources/js/Pages/Admin/Pesanan.vue
@@ -6,7 +6,34 @@
             <h1 class="text-[2rem] font-bold">Pesanan</h1>
         </template>
         <div class="py-12">
-            <h1 class="text-center">Selamat Datang di Diger!</h1>
+            <div class="grid grid-cols-2 gap-4">
+                <div
+                    class="bg-white border border-[#F1F1F1] rounded-[1.25rem] p-8 text-center"
+                >
+                    <h1 class="font-medium">Total Kreator</h1>
+                    <h1 class="text-[2rem] font-semibold mt-14">289</h1>
+                </div>
+                <div
+                    class="bg-white border border-[#F1F1F1] rounded-[1.25rem] p-8 text-center"
+                >
+                    <h1 class="font-medium">Total Produk</h1>
+                    <h1 class="text-[2rem] font-semibold mt-14">511</h1>
+                </div>
+                <div
+                    class="bg-white border border-[#F1F1F1] rounded-[1.25rem] p-8 text-center"
+                >
+                    <h1 class="font-medium">Produk Terjual</h1>
+                    <h1 class="text-[2rem] font-semibold mt-14">1230</h1>
+                </div>
+                <div
+                    class="bg-white border border-[#F1F1F1] rounded-[1.25rem] p-8 text-center"
+                >
+                    <h1 class="font-medium">Total Transaksi</h1>
+                    <h1 class="text-[2rem] font-semibold mt-14">
+                        Rp. 2,065,320,000
+                    </h1>
+                </div>
+            </div>
         </div>
     </AdminLayout>
 </template>
