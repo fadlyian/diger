f90312c1c17fb2522979f7190c42de5bf74974f9
fadlyian <mansyuraw24@gmail.com>
2023-07-20 20:12:31 +0700

    lupa git add DashboardController

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
+
+class DashboardController extends Controller
+{
+    public function index(){
+        return Inertia::render('Dashboard');
+    }
+
+    public function myProduct(){
+        $data = Product::where('user_id', Auth::user()->id)->get();
+
+        return $data;
+    }
+
+    public function pustaka(){
+        return Inertia::render('Pustaka');
+    }
+
+    public function pengaturan(){
+        return Inertia::render('Setting');
+    }
+}
